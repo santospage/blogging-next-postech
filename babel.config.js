4267cfa3cf246aa98307d1dcbe60b0580a48e819
Alessandro <alessandro.siga@gmail.com>
2024-09-25 12:09:42 -0300

    Description: This pull request add Babel configurations
    Changes included:
    - Babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-syntax-import-assertions',
+    // Remova um dos plugins ou ajuste conforme necessário
+    // '@babel/plugin-syntax-import-attributes'
+  ],
+};
